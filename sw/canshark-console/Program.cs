35930b9117366426effb594c3e34027792bc3519
Frantisek Burian <BuFran@seznam.cz>
2013-09-18 14:22:17 +0200

    Add ignored files Ordered using

--- sw/canshark-console/Program.cs
+++ sw/canshark-console/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 
