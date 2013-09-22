256fbf9067eefd2fac6c7218b92bbda08c3d9c5c
Frantisek Burian <BuFran@seznam.cz>
2013-09-22 19:58:26 +0200

    [canshark] Rewritten to have shared code for canshark and canshark-gui

--- sw/canshark-console/Program.cs
+++ sw/canshark-console/Program.cs
@@ -4,133 +4,54 @@ using System.IO;
 using System.IO.Pipes;
 using System.Net;
 using System.Net.Sockets;
+using Wireshark;
 
 namespace canshark
 {
     class Program
     {
-        static NamedPipeServerStream wspipe;
-        static BinaryWriter ws;
-
         static void Main(string[] args)
         {
-            try
+            using (WiresharkPipe wireshark = new WiresharkPipe())
             {
-                wspipe = new NamedPipeServerStream("wireshark", PipeDirection.Out);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error opening pipe. Msg = " + e.Message);
-                Environment.Exit(0);
-            }
+                UdpClient ucl = new UdpClient(6000);             
 
-            UdpClient ucl = new UdpClient(6000);
-            IAsyncResult iar = ucl.BeginReceive(null, null);
+                Console.WriteLine("Waiting for connection to wireshark.");
 
-            // wait for wireshark to connect to pipe
-            Console.WriteLine("Waiting for connection to wireshark.");
-            Console.WriteLine(@"Run wireshark -k -i \\.\pipe\wireshark");
-            Process.Start(@"C:\program files\wireshark\wireshark.exe", @"-k -i \\.\pipe\wireshark");
-            wspipe.WaitForConnection();
-            Console.WriteLine("Client connected.");
+                Process.Start(@"C:\program files\wireshark\wireshark.exe", @"-k -i \\.\pipe\wireshark");
 
-            // connect binary writer to pipe to write binary data into it
-            ws = new BinaryWriter(wspipe);
+                wireshark.WaitForConnection();
 
-            
-            WiresharkHeader();
+                Console.WriteLine("Client connected.");
 
-            // run forever
-            while (true)
-            {
-                // loop through entire length and write data into wireshark
-                try
+                wireshark.WriteHeader(WiresharkPipe.DLT_USER0, 16);
+
+                IAsyncResult iar = ucl.BeginReceive(null, null);
+
+                
+                // run forever
+                while (wireshark.Connected)
                 {
-                    byte[] msg = new byte[0x20];
-                    if (iar.AsyncWaitHandle.WaitOne(1000))
+                    if (!iar.AsyncWaitHandle.WaitOne(1000))
+                        continue;
+                    
+                    IPEndPoint ep = new IPEndPoint(0, 0);
+                    byte[] data = ucl.EndReceive(iar, ref ep);
+                    iar = ucl.BeginReceive(null, 0);
+
+                    using (MemoryStream ms = new MemoryStream(data))
+                    using (BinaryReader br = new BinaryReader(ms))
                     {
-                        IPEndPoint ep = new IPEndPoint(0,0);
-                        byte[] data = ucl.EndReceive(iar, ref ep);
-                        iar = ucl.BeginReceive(null, 0);
-
-                        for (int i = 0; (i + 0x20) <= data.Length; i+=0x20)
+                        while (ms.Position < ms.Length)
                         {
-                            Array.Copy(data, i, msg, 0, 0x20);
-                            WiresharkFrame(msg);
+                            CanMessage m = CanMessage.DeserializeFrom(br);
+
+                            wireshark.WriteFrame(m.Sec, m.Usec, m);
                         }
-                    }
+                    }                                        
                 }
-                catch
-                {
-                    Console.WriteLine("Pipe has been closed.");
-                    break;
-                }
-            }
-            ucl.Close();
-
-        }
-
-        static void WiresharkHeader()
-        {
-            uint magic_num = 0xa1b2c3d4;    // used for endianness
-            short version_major = 2;        // version
-            short version_minor = 4;        // version
-            int thiszone = 0;               // zone (unused)
-            uint sigfigs = 0;               // significant figures (unused)
-            uint snaplen = 16;           // snapshot length (max value)
-            uint network = 147;//227;             // DLT_CAN_SOCKETCAN
-
-            try
-            {
-                ws.Write(magic_num);
-                ws.Write(version_major);
-                ws.Write(version_minor);
-                ws.Write(thiszone);
-                ws.Write(sigfigs);
-                ws.Write(snaplen);
-                ws.Write(network);
-            }
-            catch { }
-        }
-
-        static void WiresharkFrame(byte[] candata)
-        {
-            long sec = 0, usec = 0;
-
-            UInt64 ticks = candata[0x18];
-            ticks |= (UInt64)candata[0x19] << 8;
-            ticks |= (UInt64)candata[0x1A] << 16;
-            ticks |= (UInt64)candata[0x1B] << 24;
-            ticks |= (UInt64)candata[0x1C] << 32;
-            ticks |= (UInt64)candata[0x1D] << 40;
-            ticks |= (UInt64)candata[0x1E] << 48;
-            ticks |= (UInt64)candata[0x1F] << 56;
-
-            sec = (long)(ticks / (1000*65536));
-            usec = (long)(ticks % (1000*65536)) * 1000 / 65536;
-            
-            // write packet header to wireshark
-            ws.Write((uint)sec);
-            ws.Write((uint)usec);
-            ws.Write(16);   // len
-            ws.Write(16);   // len
-
-            // mob-id
-            ws.Write(candata[3]);
-            ws.Write(candata[2]);
-            ws.Write(candata[1]);
-            ws.Write(candata[0]);
-
-            // length
-            ws.Write(candata[16]);
-            ws.Write(candata[6]);   // Source
-
-            // time
-            ws.Write(candata[5]);
-            ws.Write(candata[4]);
-
-            // DATA
-            ws.Write(candata, 8, 8);
+                ucl.Close();
+            }            
         }
     }
 }
---
+++ sw/canshark-console/Wireshark.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using Wireshark.Pcap;
+
+namespace Wireshark
+{
+    public interface ISerializer
+    {
+        int SerializeLen();
+        void SerializeTo(BinaryWriter bw);
+    }
+
+    class WiresharkPipe : IDisposable
+    {
+        public const uint DLT_USER0 = 147;
+
+        private NamedPipeServerStream _pipe;
+        static BinaryWriter _bw;
+
+        public WiresharkPipe()
+        {
+            try
+            {
+                _pipe = new NamedPipeServerStream("wireshark", PipeDirection.Out);
+            }
+            catch
+            {
+                _pipe = null;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_bw != null)
+                _bw.Dispose();
+
+            if (_pipe != null)
+                _pipe.Dispose();
+        }
+
+        public bool Connected { get { return _bw != null; } }
+
+        public void WaitForConnection()
+        {
+            _pipe.WaitForConnection();
+
+            // connect binary writer to pipe to write binary data into it
+            _bw = new BinaryWriter(_pipe);
+        }
+
+
+        public void WriteHeader(uint network, uint snaplen) // DLT_USER0 = 147
+        {
+            if (_bw == null)
+                return;
+
+            try
+            {
+                _bw.Write((UInt32)0xa1b2c3d4);   // MAGIC
+                _bw.Write((UInt16)2);            // Ver major
+                _bw.Write((UInt16)4);            // Ver minot
+                _bw.Write((UInt32)0);            // ZONE
+                _bw.Write((UInt32)0);            // significant figures
+                _bw.Write((UInt32)snaplen);
+                _bw.Write((UInt32)network);
+            }
+            catch 
+            {
+                _bw.Dispose();
+                _bw = null;
+            }
+        }
+
+        public void WriteFrame(UInt32 sec, UInt32 usec, ISerializer candata)
+        {
+            if (_bw == null)
+                return;
+
+            try
+            {
+                new PCAPRH(sec, usec, candata.SerializeLen()).SerializeTo(_bw);
+                candata.SerializeTo(_bw);
+            }
+            catch
+            {
+                _bw.Dispose();
+                _bw = null;
+            }
+        }
+
+
+        
+    }
+}
