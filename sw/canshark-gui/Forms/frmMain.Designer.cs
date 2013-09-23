8740656fb8b575c5b6f9d2f543ffd308fc761ae1
Frantisek Burian <BuFran@seznam.cz>
2013-09-23 10:40:04 +0200

    [canshark-gui] Add first version of GUI

---
+++ sw/canshark-gui/Forms/frmMain.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace canshark_gui
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.nframes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frames:";
+            // 
+            // nframes
+            // 
+            this.nframes.AutoSize = true;
+            this.nframes.Location = new System.Drawing.Point(176, 54);
+            this.nframes.Name = "nframes";
+            this.nframes.Size = new System.Drawing.Size(13, 13);
+            this.nframes.TabIndex = 1;
+            this.nframes.Text = "0";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 508);
+            this.Controls.Add(this.nframes);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "canshark-gui";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nframes;
+    }
+}
+
---
+++ sw/canshark-gui/Forms/frmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace canshark_gui
+{
+    public partial class frmMain : Form
+    {
+        int frames = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            nframes.Text = frames.ToString();
+        }
+    }
+}
