6dd7ac2447b89d986c795d677cc5e6289113f251
Frantisek Burian <BuFran@seznam.cz>
2014-02-11 05:55:16 +0100

    Updated to the new libopencm3, beeter build process

---
+++ fw/canshark/libopencm3-config.mk
@@ -0,0 +1,22 @@
+##
+## This file is part of the canshark project.
+##
+## This library is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this library.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+CPPFLAGS	+= -I$(OPENCM3_DIR)include
+LDFLAGS		+= -L$(OPENCM3_DIR)lib
+OPENCM3_LIBDEP	:= $(OPENCM3_DIR)lib/lib$(OPENCM3_LIBNAME).a
+LIBDEPS		+= $(OPENCM3_LIBDEP)
+LDLIBS		+= -l$(OPENCM3_LIBNAME)
